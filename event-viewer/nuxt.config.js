9bde4849675f2a07a97f890998ea85e1ce106080
ryntab <57367794+ryntab@users.noreply.github.com>
2024-06-10 21:37:30 -0400

    fix: :bug: fix redirect in module configuration

--- event-viewer/nuxt.config.js
+++ event-viewer/nuxt.config.js
@@ -11,37 +11,4 @@ export default defineNuxtConfig({
       SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN
     }
   },
-  app: {
-    baseURL: '/ProcessWireSentry/event-viewer/dist/', // Base URL for the application
-    buildAssetsDir: '/_nuxt/', // Directory for build assets
-  },
-  build: {
-    publicPath: '/ProcessWireSentry/event-viewer/dist/',
-    splitChunks: {
-      layouts: false,
-      pages: false,
-      commons: false
-    },
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.(css|vue)$/,
-            chunks: 'all',
-            enforce: true
-          }
-        }
-      }
-    }
-
-  },
-  router: {
-    base: '/ProcessWireSentry/event-viewer/dist/'
-  },
-  // build: {
-  //   transpile: ['@vitejs/plugin-vue']
-  // },
-  // vite: true
 })
